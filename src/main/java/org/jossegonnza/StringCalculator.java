479e056e6077d36ee953c5ac7d630e608ea5015f
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-26 15:06:51 +0100

    🟢 Should return the number if it is only one

--- src/main/java/org/jossegonnza/StringCalculator.java
+++ src/main/java/org/jossegonnza/StringCalculator.java
@@ -2,6 +2,6 @@ package org.jossegonnza;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return Integer.parseInt(null);
+        return Integer.parseInt(numbers);
     }
 }
