8458bd36d095a46b74e414a9505b630c8bf7ce99
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-26 15:02:12 +0100

    🔴 Should return the number

--- src/test/java/org/jossegonnza/StringCalculatorTest.java
+++ src/test/java/org/jossegonnza/StringCalculatorTest.java
@@ -1,6 +1,15 @@
 package org.jossegonnza;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
+
+    @Test
+    public void should_return_the_number_if_it_is_only_one() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        Assertions.assertThat(stringCalculator.add("1")).isEqualTo(1);
+    }
 
 }
