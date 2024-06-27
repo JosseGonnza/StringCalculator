5f842b91c6726cbba76c5736ed026545746f6edc
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-27 21:53:15 +0100

    🟢 Should return the number of string

--- src/test/java/org/jossegonnza/StringCalculatorTest.java
+++ src/test/java/org/jossegonnza/StringCalculatorTest.java
@@ -13,4 +13,10 @@ class StringCalculatorTest {
         Assertions.assertThat(stringCalculator.add("")).isEqualTo(0);
     }
 
+    @Test
+    public void should_return_the_number_of_string() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        Assertions.assertThat(stringCalculator.add("4")).isEqualTo(4);
+    }
 }
