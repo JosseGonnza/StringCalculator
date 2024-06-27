19086b79d0a9e2d5dca2b812b8dee2fa3c34a3e1
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-27 21:37:51 +0100

    🔴 Pass red test

--- src/test/java/org/jossegonnza/StringCalculatorTest.java
+++ src/test/java/org/jossegonnza/StringCalculatorTest.java
@@ -1,6 +1,16 @@
 package org.jossegonnza;
 
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class StringCalculatorTest {
+
+    @Test
+    public void should_return_0_if_string_is_empty() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        Assertions.assertThat(stringCalculator.add("")).isEqualTo(0);
+    }
 
 }
